bf6c5f4c00b5ce043f0c0f7c1abb42958b88e25a
n3tw4lk3r <piroman.pasha@yandex.ru>
2026-02-03 19:41:14 +0300

    Make 'private' ProcessingServer_accept_connection

--- include/core/ProcessingServer.h
+++ include/core/ProcessingServer.h
@@ -9,8 +9,6 @@ typedef struct ProcessingServer ProcessingServer;
 ProcessingServer *ProcessingServer_create(int port, int *error_flag);
 void ProcessingServer_destroy(ProcessingServer *server);
 
-int ProcessingServer_accept_connection(int listen_fd, struct sockaddr_in *client_address, int *error_flag);
-
 void ProcessingServer_run(ProcessingServer *server);
 void ProcessingServer_attach_client(ProcessingServer *server, int file_descriptor, struct sockaddr_in *address);
 void Processing_server_detach_client(ProcessingServer *server, int file_descriptor);

b47ac9e8e5eb4d9563ee1e47f4598058240dcb69
n3tw4lk3r <piroman.pasha@yandex.ru>
2026-02-03 19:44:08 +0300

    Make 'private' Client_receive

--- include/core/Client.h
+++ include/core/Client.h
@@ -10,6 +10,4 @@ void Client_destroy(Client *client);
 void Client_connect(Client *client, int *error_flag);
 int Client_is_connected(const Client *client);
 
-ssize_t Client_receive(Client *client, char *buffer, size_t buffer_size, int *error_flag);
-
 void Client_run(Client *client, int *error_flag);
